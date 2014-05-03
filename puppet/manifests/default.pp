10b1850540ada80390f03ea9701a92736a3c26ee
twstddev <twstd.dev@gmail.com>
2014-05-03 18:02:54 +0100

    Install ruby and rails

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -42,3 +42,43 @@ package { "nodejs":
 package { [ "sqlite3", "libsqlite3-dev" ]:
 	ensure => "present",
 }
+
+# Rails installation
+# This installation follows instruction from https://www.digitalocean.com/community/articles/how-to-install-ruby-on-rails-on-ubuntu-12-04-lts-precise-pangolin-with-rvm
+
+exec { "install_rvm":
+	command => "${execute_as_vagrant} 'curl -L https://get.rvm.io | bash -s stable'"
+}
+
+exec { "source_rvm_profile":
+	command => "${execute_as_vagrant} 'source ${home}/.rvm/scripts/rvm'"
+}
+
+exec { "install_dependencies":
+	command => "${execute_as_vagrant} 'rvm requirements'"
+}
+
+exec { "install_ruby":
+	command => "${execute_as_vagrant} 'rvm install ruby && rvm use ruby --default'"
+}
+
+exec { "install_rubygems":
+	command  => "${execute_as_vagrant} 'rvm rubygems current'"
+}
+
+exec { "disable_documentation":
+	command => "${execute_as_vagrant} 'echo \"gem: --no-ri --no-rdoc\" > ${home}/.gemrc'"
+}
+
+exec { "install_rails":
+	command => "${execute_as_vagrant} 'gem install rails'"
+}
+
+Package[ "curl" ] ->
+	Exec[ "install_rvm" ] ->
+	Exec[ "source_rvm_profile" ] ->
+	Exec[ "install_dependencies" ] ->
+	Exec[ "install_ruby" ] ->
+	Exec[ "install_rubygems" ] ->
+	Exec[ "disable_documentation" ] ->
+	Exec[ "install_rails" ]

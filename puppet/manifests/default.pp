e86e87216b694736f6b9615e4ce2367eea10639d
twstddev <twstd.dev@gmail.com>
2014-05-03 17:06:12 +0100

    Update system on initialisation

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -2,3 +2,16 @@
 Exec {
 	path => [ "/usr/bin", "/usr/local/bin" ]
 }
+
+# Prepare system before main stage
+stage { "init": }
+
+class update_apt {
+	exec { "apt-get -y update": }
+}
+
+class{ "update_apt" :
+	stage => init,
+}
+
+Stage[ "init" ] -> Stage[ "main" ]
